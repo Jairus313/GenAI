1f76cdd70c8829f2fbdf868c10a64d4fee31d645
Jairus313 <sudeep.nellue.7@gmail.com>
2026-01-11 16:33:33 +0530

    building service for gemini ai

---
+++ phase_1/llm_service.py
@@ -0,0 +1,35 @@
+# app/services/llm_service.py
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class GeminiService:
+    def __init__(self, api_key: str):
+        genai.configure(api_key=api_key)
+        self.model = genai.GenerativeModel("gemini-3-flash-preview")
+
+    def generate(self, prompt: str) -> str:
+        response = self.model.generate_content(
+            prompt, generation_config={"temperature": 0.2}  # low randomness
+        )
+        return response.text
+
+
+gemini_client = GeminiService(os.getenv("GEMINI_API_KEY"))
+
+prompt = """
+        teach about the importance of LLMs and why professional needs to be learning it
+        in this year with their jobs, explain it in reason, need and conclusion but 
+        keep it short and concise.
+"""
+
+gemini_response = gemini_client.generate(prompt)
+
+print(f"\n\nGemini Response:\n{gemini_response}")
